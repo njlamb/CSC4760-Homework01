0a900bd1889bf920a5a9e70f88e04fd8ba060af2
njlamb <142761756+njlamb@users.noreply.github.com>
2024-03-07 23:13:29 -0600

    updated problem 4

--- example/homework/04/init.cpp
+++ example/homework/04/init.cpp
@@ -1,15 +1,27 @@
 #include <Kokkos_Core.hpp>
 #include <cstdio>
+#include <iostream>
 
+using namespace std;
 // Do simple parallel reduce to output the maximum element in a View
 
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
   // Make View and create values
-  
-  // Do a parallel reduction
-  
+  int lval;
+  Kokkos::View<int*> view_4("view_4", 10);
+
+  for(int i=0; i<view_4.extent(0); i++){
+    view_4(i) = i*i;
   }
+  // Do a parallel reduction
+  Kokkos::parallel_reduce("largest value", view_4.extent(0), KOKKOS_LAMBDA(const int& i, int& val){
+    val = (view_4(i) > val) ? view_4(i) : val;
+  }, Kokkos::Max<int>(lval));
+  Kokkos::fence();
+  cout << "The largest value in the view: " << lval << endl;
+  }
+  
   Kokkos::finalize();
 }
