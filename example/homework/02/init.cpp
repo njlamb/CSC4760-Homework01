e21eb534e665844b951bf1025e4bad01d220b136
njlamb <142761756+njlamb@users.noreply.github.com>
2024-03-07 22:28:03 -0600

    updated problem 2

--- example/homework/02/init.cpp
+++ example/homework/02/init.cpp
@@ -1,5 +1,6 @@
 #include <Kokkos_Core.hpp>
 #include <cstdio>
+#include <iostream>
 
 // Problem: Make an n ∗ m View where each index equals 1000 ∗ i ∗ j
 
@@ -7,10 +8,21 @@ int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
   // set n and m, you can change these values
-  int n,m = 16;
+  int n = 4;
+  int m = 4;
   // Make View
-  
+  Kokkos::View<int**> myView("myView", n, m);
   // set values to 1000 * i * j;
+  for(int i=0; i<myView.extent(0); i++){
+    for(int j=0; j<myView.extent(1); j++){
+      myView(i,j) = 1000 * i * j;
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      std::cout << "View at index " << i << "," << j << ": " << myView(i,j) << std::endl;
+    }
+  }
   }
   Kokkos::finalize();
 }
